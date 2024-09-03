24553f23864f08b82a2c88e4a9910a786bedbc29
Ricardo JL Rufino <ricardo.jl.rufino@gmail.com>
2024-09-03 16:32:06 -0300

    fix invalid import

--- functions/imports.sh
+++ functions/imports.sh
@@ -2,7 +2,6 @@
 export PATH="$PATH:$SCRIPT_DIR/functions/cmds"
 . $SCRIPT_DIR/functions/colors.sh
 . $SCRIPT_DIR/functions/requirements_utils.sh
-. $SCRIPT_DIR/functions/pkg_install.sh
 . $SCRIPT_DIR/functions/configuration.sh
 . $SCRIPT_DIR/functions/ask.sh
 
